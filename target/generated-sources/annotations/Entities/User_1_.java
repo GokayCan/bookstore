c6df49e6b021f3d4859f8a8c7b9ae09e25ea5599
GokayCan <gokaycan_kurtoglu@hotmail.com>
2022-11-28 21:01:02 +0300

    4

--- target/generated-sources/annotations/Entities/User_1_.java
+++ target/generated-sources/annotations/Entities/User_1_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-11-05T12:36:17")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-11-28T19:56:22")
 @StaticMetamodel(User_1.class)
 public class User_1_ { 
 
